4fb8154526a10ade7b1572194aa4476bfee21284
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-18 18:05:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,11 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  holiday_hash.each do |season,holiday|
-    if holiday == :fourth_of_july
-      :fourth_of_july.values.first
-    end
-  end
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
