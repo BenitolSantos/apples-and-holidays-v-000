138e9239d09c9c91e579e5bb0e37f4846b07c336
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-18 20:07:28 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season,holiday|
     holiday.each do |holiday_name,supply_array|
       if supply_array.include?("BBQ")
-        << holiday_name 
+        holidays_with_bbqs << holiday_name
       else
       end
     end
