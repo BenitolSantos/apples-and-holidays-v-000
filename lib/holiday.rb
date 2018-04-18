c409a6e3733e9076217a33bc300df1ffc5be18c3
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-18 17:57:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,6 +20,10 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # return the second element in the 4th of July array
   holiday_hash.each do |season,holiday|
     holiday.each do |supplies_array|
+      if supplies_array == :fourth_of_july
+        :fourth_of_july.values.first
+      end
+    end
   end
 end
 
