615be497513ba576f5d83ec66eff48d250c0dcf7
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-18 19:18:13 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
    #reminder! "#{}"
     holiday.each do |holiday_name,supplies| #pry and hashes go together like bacon and eggs
       puts "#{season.capitalize}:"
-      puts " #{holiday_name.capitalize}: " + "#{supplies.join(", ")}"
+      puts "  #{holiday_name.capitalize}: " + "#{supplies.join(", ")}"
     end
   end
 end
