c1e7c82b3c81d15d35b5df955784377b4dd93774
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-18 19:11:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,8 +67,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     print "#{season.capitalize}:"  #reminder! "#{}"
     holiday.each do |holiday_name,supplies| #pry and hashes go together like bacon and eggs
-      supplies.each do |supply|
-        binding.pry
+      supplies.join(,)
+        
       end
     end
   end
